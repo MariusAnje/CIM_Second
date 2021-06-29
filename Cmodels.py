0977f9e0394becffd97b00a65f4f6a61101e87d6
MariusAnje <mariusanje@outlook.com>
2021-06-29 15:44:47 -0400

    cmodules

---
+++ Cmodels.py
@@ -0,0 +1,105 @@
+import torch
+from torch import nn
+from Functions import SCrossEntropyLossFunction
+from Cmodules import SLinear, SReLU, SModel, SConv2d, SMaxpool2D
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(28*28,32)
+        self.fc2 = nn.Linear(32,32)
+        self.fc3 = nn.Linear(32,10)
+        self.relu = nn.ReLU()
+    
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class SMLP3(SModel):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = SLinear(28*28,32)
+        self.fc2 = SLinear(32,32)
+        self.fc3 = SLinear(32,10)
+        self.relu = SReLU()
+    
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        xS = torch.zeros_like(x)
+        x, xS = self.fc1(x, xS)
+        x, xS = self.relu(x, xS)
+        x, xS = self.fc2(x, xS)
+        x, xS = self.relu(x, xS)
+        x, xS = self.fc3(x, xS)
+        return x, xS
+
+class SMLP4(SModel):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = SLinear(28*28,32)
+        self.fc2 = SLinear(32,32)
+        self.fc3 = SLinear(32,32)
+        self.fc4 = SLinear(32,10)
+        self.relu = SReLU()
+    
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        xS = torch.zeros_like(x)
+        x, xS = self.fc1(x, xS)
+        x, xS = self.relu(x, xS)
+        x, xS = self.fc2(x, xS)
+        x, xS = self.relu(x, xS)
+        x, xS = self.fc3(x, xS)
+        x, xS = self.relu(x, xS)
+        x, xS = self.fc4(x, xS)
+        return x, xS
+
+
+class SLeNet(SModel):
+
+    def __init__(self):
+        super().__init__()
+        # 1 input image channel, 6 output channels, 3x3 square convolution
+        # kernel
+        self.conv1 = SConv2d(1, 6, 3, padding=1)
+        self.conv2 = SConv2d(6, 16, 3, padding=1)
+        # an affine operation: y = Wx + b
+        self.fc1 = SLinear(16 * 7 * 7, 120)  # 6*6 from image dimension
+        self.fc2 = SLinear(120, 84)
+        self.fc3 = SLinear(84, 10)
+        self.pool = SMaxpool2D(2)
+        self.relu = SReLU()
+
+    def forward(self, x):
+        xS = torch.zeros_like(x)
+        x, xS = self.conv1(x, xS)
+        x, xS = self.relu(x, xS)
+        x, xS = self.pool(x, xS)
+        
+        x, xS = self.conv2(x, xS)
+        x, xS = self.relu(x, xS)
+        x, xS = self.pool(x, xS)
+        
+        x = x.view(-1, self.num_flat_features(x))
+        if xS is not None:
+            xS = xS.view(-1, self.num_flat_features(xS))
+        
+        x, xS = self.fc1(x, xS)
+        x, xS = self.relu(x, xS)
+        
+        x, xS = self.fc2(x, xS)
+        x, xS = self.relu(x, xS)
+        
+        x, xS = self.fc3(x, xS)
+        return x, xS
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
---
+++ Cmodules.py
@@ -0,0 +1,227 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from Functions import SLinearFunction, SConv2dFunction, SMSEFunction, SCrossEntropyLossFunction    
+
+class CModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def create_helper(self):
+        self.weightS = torch.ones(self.op.weight.size())
+        self.noise = torch.zeros_like(self.op.weight)
+        self.mask = torch.ones_like(self.op.weight)
+
+    def set_noise(self, var):
+        self.noise = torch.normal(mean=0., std=var, size=self.noise.size()).to(self.op.weight.device)
+    
+    def clear_noise(self):
+        self.noise = torch.zeros_like(self.op.weight)
+    
+    def set_mask(self, portion, mode):
+        if mode == "portion":
+            th = self.weightS.grad.abs().view(-1).quantile(1-portion)
+            self.mask = (self.weightS.abs() <= th).to(torch.float)
+        elif mode == "th":
+            self.mask = (self.weightS.abs() <= portion).to(torch.float)
+        else:
+            raise NotImplementedError(f"Mode: {mode} not supported, only support mode portion & th, ")
+    
+    def set_mask_mag(self, portion, mode):
+        if mode == "portion":
+            th = self.op.weight.abs().view(-1).quantile(1-portion)
+            self.mask = (self.op.weight.data.abs() <= th).to(torch.float)
+        elif mode == "th":
+            self.mask = (self.op.weight.data.abs() <= portion).to(torch.float)
+        else:
+            raise NotImplementedError(f"Mode: {mode} not supported, only support mode portion & th, ")
+    
+    def set_mask_sail(self, portion, mode):
+        # saliency = self.weightS.abs() * (self.op.weight.data ** 2)
+        saliency = self.weightS.abs() / (self.op.weight.data ** 2 + 1e-8)
+        if mode == "portion":
+            th = saliency.view(-1).quantile(1-portion)
+            self.mask = (saliency <= th).to(torch.float)
+        elif mode == "th":
+            self.mask = (saliency <= portion).to(torch.float)
+        else:
+            raise NotImplementedError(f"Mode: {mode} not supported, only support mode portion & th, ")
+    
+    def clear_mask(self):
+        self.mask = torch.ones_like(self.op.weight)
+
+    def push_S_device(self):
+        self.weightS = self.weightS.to(self.op.weight.device)
+        self.mask = self.mask.to(self.op.weight.device)
+
+    def clear_S_grad(self):
+        self.weightS = torch.zeros_like(self.op.weight)
+    
+    def fetch_S_grad(self):
+        return (self.weightS.abs() * self.mask).sum()
+    
+    def fetch_S_grad_list(self):
+        return (self.weightS.data * self.mask)
+
+    def do_second(self):
+        self.op.weight.grad.data = self.op.weight.grad.data / (self.weightS.data + 1e-10)
+
+class CLinear(CModule):
+    def __init__(self, in_features, out_features, bias=True):
+        super().__init__()
+        self.op = nn.Linear(in_features, out_features, bias)
+        self.create_helper()
+        self.function = F.linear
+
+    def Cbackward(self, grad_outputS):
+        BS = self.input.shape[0]
+        IN = self.input.view(BS,1,-1)
+        self.gradWS = IN.swapaxes(1,2).bmm(IN).view(BS,-1).t().mm(grad_outputS.view(BS,-1))
+        self.weightS.grad = self.gradWS.diag().view(self.weightS.shape)
+        gradIS = self.op.weight.t().matmul(grad_outputS).matmul(self.op.weight)
+        return gradIS
+    
+    def forward(self, x):
+        self.input = x
+        x1 = self.function(x, (self.op.weight + self.noise) * self.mask, self.op.bias)
+        return x1
+
+class CCrossEntropyLoss(nn.Module):
+    def __init__(self, weight=None, size_average=None, ignore_index=-100, reduce=None, reduction='mean'):
+        super().__init__()
+        self.op = nn.CrossEntropyLoss(weight=weight, size_average=size_average, ignore_index=ignore_index, reduce=reduce, reduction=reduction)
+    
+    def Cbackward(self, grad_outputS):
+        BS = grad_outputS.shape[0]
+        e3 = (grad_outputS - grad_outputS.max()).exp()
+        print(e3.shape)
+        e3_sum = e3.sum(dim=1).unsqueeze(1).expand_as(e3)
+        ratio = (e3 / e3_sum).view(BS,1,-1)
+        return (torch.diag_embed(ratio.view(BS,-1),0,1) - ratio.swapaxes(1,2).bmm(ratio))
+
+    def forward(self, input, labels):
+        output = self.op(input, labels)
+        return output
+
+
+class CConv2d(CModule):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'):
+        super().__init__()
+        self.op = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias, padding_mode)
+        self.create_helper()
+        self.function = SConv2dFunction.apply
+
+    def forward(self, x, xS):
+        x, xS = self.function(x, xS, (self.op.weight + self.noise) * self.mask, self.weightS, self.op.bias, self.op.stride, self.op.padding, self.op.dilation, self.op.groups)
+        return x, xS
+
+class CReLU(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.op = nn.ReLU()
+    
+    def forward(self, x, xS):
+        return self.op(x), self.op(xS)
+
+class SMaxpool2D(nn.Module):
+    def __init__(self, kernel_size, stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False):
+        super().__init__()
+        return_indices = True
+        self.op = nn.MaxPool2d(kernel_size, stride, padding, dilation, return_indices, ceil_mode)
+    
+    def parse_indice(self, indice):
+        bs, ch, w, h = indice.shape
+        length = w * h
+        BD = torch.LongTensor(list(range(bs))).expand([length,ch,bs]).swapaxes(0,2).reshape(-1)
+        CD = torch.LongTensor(list(range(ch))).expand([bs,length,ch]).swapaxes(1,2).reshape(-1)
+        shape = [bs, ch, -1]
+        return shape, [BD, CD, indice.view(-1)]
+
+    
+    def forward(self, x, xS):
+        x, indices = self.op(x)
+        shape, indices = self.parse_indice(indices)
+        xS = xS.view(shape)[indices].view(x.shape)
+        return x, xS
+
+class CModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def push_S_device(self):
+        for m in self.modules():
+            if isinstance(m, CModule):
+                m.push_S_device()
+
+    def clear_S_grad(self):
+        for m in self.modules():
+            if isinstance(m, CModule):
+                m.clear_S_grad()
+
+    def do_second(self):
+        for m in self.modules():
+            if isinstance(m, CModule):
+                m.do_second()
+
+    def fetch_S_grad(self):
+        S_grad_sum = 0
+        for m in self.modules():
+            if isinstance(m, CModule):
+                S_grad_sum += m.fetch_S_grad()
+        return S_grad_sum
+    
+    def calc_S_grad_th(self, quantile):
+        S_grad_list = None
+        for m in self.modules():
+            if isinstance(m, CModule):
+                if S_grad_list is None:
+                    S_grad_list = m.fetch_S_grad_list().view(-1)
+                else:
+                    S_grad_list = torch.cat([S_grad_list, m.fetch_S_grad_list().view(-1)])
+        th = torch.quantile(S_grad_list, 1-quantile)
+        # print(th)
+        return th
+    
+    def calc_sail_th(self, quantile):
+        sail_list = None
+        for m in self.modules():
+            if isinstance(m, CModule):
+                # sail = (m.fetch_S_grad_list().abs() * (m.op.weight.data**2)).view(-1)
+                sail = (m.fetch_S_grad_list().abs() / (m.op.weight.data**2 + 1e-8)).view(-1)
+                if sail_list is None:
+                    sail_list = sail
+                else:
+                    sail_list = torch.cat([sail_list, sail])
+        th = torch.quantile(sail_list, 1-quantile)
+        # print(th)
+        return th
+
+    def set_noise(self, var):
+        for m in self.modules():
+            if isinstance(m, CModule):
+                m.set_noise(var)
+    
+    def clear_noise(self):
+        for m in self.modules():
+            if isinstance(m, CModule):
+                m.clear_noise()
+    
+    def set_mask(self, th, mode):
+        for m in self.modules():
+            if isinstance(m, CModule):
+                m.set_mask(th, mode)
+    
+    def set_mask_mag(self, th, mode):
+        for m in self.modules():
+            if isinstance(m, CModule):
+                m.set_mask_mag(th, mode)
+    
+    def set_mask_sail(self, th, mode):
+        for m in self.modules():
+            if isinstance(m, CModule):
+                m.set_mask_sail(th, mode)
+    
+    def clear_mask(self):
+        for m in self.modules():
+            if isinstance(m, CModule):
+                m.clear_mask()
